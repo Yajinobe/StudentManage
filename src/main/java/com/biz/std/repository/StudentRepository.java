9e407822f63929d176914a093253e6d39e6b4708
Yajinobe <35264487+Yajinobe@users.noreply.github.com>
2018-02-26 17:23:16 +0800

    Update StudentRepository.java

--- src/main/java/com/biz/std/repository/StudentRepository.java
+++ src/main/java/com/biz/std/repository/StudentRepository.java
@@ -3,7 +3,6 @@ package com.biz.std.repository;
 import com.biz.std.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student,Integer>,JpaSpecificationExecutor<Student> {
 	
